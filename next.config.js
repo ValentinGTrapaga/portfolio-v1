c6434d85399c6a093f2037f06e3e9a8e7d3451db
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2022-10-01 23:32:56 -0300

    error

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  env: {
-    sendGrid_API_KEY:
-      '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'
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
